0485539ae670c7af1395db5fd336a0579892f4c8
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 19:45:33 +0800

    dfasfd

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,18 +1,16 @@
 package middlewares
 
 import (
-	"os"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // Cors 跨域配置
-func Cors() gin.HandlerFunc {
+func Cors(cors_domain string) gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
-	config.AllowOrigins = []string{os.Getenv("CORS_DOMAIN")}
+	config.AllowOrigins = []string{cors_domain}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
