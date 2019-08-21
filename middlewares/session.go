56147764c3ba12a30847b9fe7016ffb2ea3d181c
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 19:26:20 +0800

    asd

--- middlewares/session.go
+++ middlewares/session.go
@@ -1,14 +1,20 @@
 package middlewares
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
 )
 
 // Session 初始化session
 func Session(secret string) gin.HandlerFunc {
-	store := cookie.NewStore([]byte(secret))
-	store.Options(sessions.Options{HttpOnly: false, MaxAge: 0, Path: "/"})
+	store, err := redis.NewStore(10, "tcp", os.Getenv("REDIS_ADDR"), os.Getenv("REDIS_PASSWORD"), []byte(secret))
+	if err != nil {
+		log.Fatal(err)
+	}
+	store.Options(sessions.Options{HttpOnly: false, MaxAge: 30 * 86400, Path: "/"})
 	return sessions.Sessions("gin-session", store)
 }
