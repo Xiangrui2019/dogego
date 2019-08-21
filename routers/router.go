b6a64a805555ff384fa23140c5415c591df60282
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 22:08:26 +0800

    dsaf

--- routers/router.go
+++ routers/router.go
@@ -13,6 +13,17 @@ func NewRouter() *gin.Engine {
 	// 中间件, 顺序必须是这样
 	router.Use(middlewares.Session(os.Getenv("SESSION_SECERT")))
 	router.Use(middlewares.Cors(os.Getenv("CORS_DOMAIN")))
+	router.Use(middlewares.CurrentUser())
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.Any("/ping", nil)
+
+		auth := v1.Use(middlewares.AuthRequired())
+		{
+			auth.GET("/", nil)
+		}
+	}
 
 	return router
 }
