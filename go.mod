fca844f44ce5b7ac945befcc619e66937dc256a4
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 18:03:11 +0800

    ad database

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module dogego
 
 go 1.12
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/jinzhu/gorm v1.9.10
+	github.com/joho/godotenv v1.3.0
+)
---
+++ models/database.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"log"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase(connectionDSN string) {
+	db, err := gorm.Open("mysql", connectionDSN)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.LogMode(true)
+
+	// 连接池设置
+	//空闲
+	db.DB().SetMaxIdleConns(20)
+	//打开
+	db.DB().SetMaxOpenConns(100)
+	//超时
+	db.DB().SetConnMaxLifetime(time.Second * 30)
+
+	DB = db
+}
