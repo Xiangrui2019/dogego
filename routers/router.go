ae6d5cfcb26d7e1b2494188e67612a3684d59304
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 19:29:27 +0800

    register session middleware

--- routers/router.go
+++ routers/router.go
@@ -1,9 +1,17 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"dogego/middlewares"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
+
+	// 中间件, 顺序必须是这样
+	router.Use(middlewares.Session(os.Getenv("SESSION_SECERT")))
 
 	return router
 }
