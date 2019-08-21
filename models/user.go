d5600691c3f0fe274599f25ee80b73cf485002b2
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 20:55:11 +0800

    daf

--- models/user.go
+++ models/user.go
@@ -11,3 +11,13 @@ type User struct {
 	Status      string
 	Avatar      string
 }
+
+const (
+	PasswordCost = 12
+	// Active 激活用户
+	Active string = "active"
+	// Inactive 未激活用户
+	Inactive string = "inactive"
+	// Suspend 被封禁用户
+	Suspend string = "suspend"
+)
