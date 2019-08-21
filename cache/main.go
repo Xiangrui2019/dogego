ede19ae7710a9e8ae9d5633d1be82dbaae589383
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 19:53:44 +0800

    add cache

--- cache/main.go
+++ cache/main.go
@@ -1,8 +1,25 @@
 package cache
 
-import "github.com/go-redis/redis"
+import (
+	"log"
+	"os"
+
+	"github.com/go-redis/redis"
+)
 
 var CacheClient *redis.Client
 
 func ConnectRedisCache() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+	})
+
+	_, err := client.Ping().Result()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CacheClient = client
 }
