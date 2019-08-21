2363d660ef013cbbdfeb61f9724d096605b2e97d
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 07:43:09 +0800

    dsdf

--- api/user.go
+++ api/user.go
@@ -1,2 +1,19 @@
 package api
 
+import "github.com/gin-gonic/gin"
+
+func UserRegister(context *gin.Context) {
+
+}
+
+func UserLogin(context *gin.Context) {
+
+}
+
+func UserMe(context *gin.Context) {
+
+}
+
+func UserLogout(context *gin.Context) {
+
+}
