de010cd32ad7bb7c35b9d2709931c41b1699a5ac
Go Go Go <xiangrui@aiursoft.com>
2019-08-21 19:20:43 +0800

    safd

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module dogego
 go 1.12
 
 require (
+	github.com/gin-contrib/sessions v0.0.1
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
 	github.com/joho/godotenv v1.3.0
--- middlewares/session.go
+++ middlewares/session.go
@@ -1,1 +1,14 @@
 package middlewares
+
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+// Session 初始化session
+func Session(secret string) gin.HandlerFunc {
+	store := cookie.NewStore([]byte(secret))
+	store.Options(sessions.Options{HttpOnly: false, MaxAge: 0, Path: "/"})
+	return sessions.Sessions("gin-session", store)
+}
