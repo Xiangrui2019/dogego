14468a725a6376a854b85a30363b887cf622f928
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 12:12:28 +0800

    add userlogin method

--- routers/router.go
+++ routers/router.go
@@ -20,6 +20,7 @@ func NewRouter() *gin.Engine {
 	{
 		v1.POST("/ping", api.Ping)
 		v1.POST("/user/register", api.UserRegister)
+		v1.POST("/user/login", api.UserLogin)
 
 		auth := v1.Use(middlewares.AuthRequired())
 		{
