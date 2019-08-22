241e4ea2f6edde835f9779a74b80bf8acc46c781
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 15:25:33 +0800

    sdaf

--- services/change_password_service.go
+++ services/change_password_service.go
@@ -1,10 +1,35 @@
 package services
 
-import "dogego/serializer"
+import (
+	"dogego/models"
+	"dogego/serializer"
+	"net/http"
+)
 
 type ChangePasswordService struct {
+	OldPassword     string `form:"old_password" json:"old_password" binding:"required,min=8,max=40"`
+	Password        string `form:"password" json:"password" binding:"required,min=8,max=40"`
+	PasswordConfirm string `form:"password_confirm" json:"password_confirm" binding:"required,min=8,max=40"`
 }
 
-func (service *ChangePasswordService) Change() *serializer.Response {
+func (service *ChangePasswordService) Valid(user *models.User) *serializer.Response {
+	if err := user.CheckPassword(service.OldPassword); err != true {
+		return &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "旧密码输入有误",
+		}
+	}
+
+	if service.PasswordConfirm != service.Password {
+		return &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "两次输入的密码不相同",
+		}
+	}
+
+	return nil
+}
+
+func (service *ChangePasswordService) Change(user *models.User) *serializer.Response {
 
 }
