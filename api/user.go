50bd448e04d6d0684ecd731d21fe746436655068
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 10:26:56 +0800

    add alls

--- api/user.go
+++ api/user.go
@@ -1,9 +1,25 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"dogego/services"
+	"dogego/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func UserRegister(context *gin.Context) {
+	service := services.UserRegisterService{}
 
+	if err := context.ShouldBind(&service); err == nil {
+		if user, err := service.Register(); err != nil {
+			context.JSON(http.StatusInternalServerError, err)
+		} else {
+			context.JSON(http.StatusOK, user)
+		}
+	} else {
+		context.JSON(http.StatusBadRequest, utils.BuildErrorResponse(err))
+	}
 }
 
 func UserLogin(context *gin.Context) {
