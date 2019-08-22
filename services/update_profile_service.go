bb9befefb6dcc522534910162d2da5919d58335f
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 17:21:43 +0800

    add task

---
+++ services/update_profile_service.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+	"dogego/models"
+	"dogego/serializer"
+)
+
+type UpdateProfileService struct {
+}
+
+func (service *UpdateProfileService) Update(user *models.User) *serializer.Response {
+
+}
