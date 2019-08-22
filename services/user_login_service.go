3a2766751012d8ca639cd18966eebb91ba5a419f
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 12:06:23 +0800

    dsf

--- services/user_login_service.go
+++ services/user_login_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"dogego/models"
 	"dogego/serializer"
+	"net/http"
 )
 
 type UserLoginService struct {
@@ -10,4 +11,22 @@ type UserLoginService struct {
 	Password    string `form:"password" json:"password" binding:"required,min=2,max=40"`
 }
 
-func (service *UserLoginService) Login() (models.User, *serializer.Response) {}
+func (service *UserLoginService) Login() (models.User, *serializer.Response) {
+	var user models.User
+
+	if err := models.DB.Where("phone_number = ?", service.PhoneNumber).First(&user).Error; err != nil {
+		return user, &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "账号或密码错误.",
+		}
+	}
+
+	if user.CheckPassword(service.Password) == false {
+		return user, &serializer.Response{
+			Code:    http.StatusBadRequest,
+			Message: "账号或密码错误.",
+		}
+	}
+
+	return user, nil
+}
