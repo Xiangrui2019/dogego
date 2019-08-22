506328368bd2faab4c1fcc4dbc90ed74d781ffbd
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 12:01:56 +0800

    add userlogin api

--- services/user_login_service.go
+++ services/user_login_service.go
@@ -6,6 +6,8 @@ import (
 )
 
 type UserLoginService struct {
+	PhoneNumber string `form:"user_name" json:"user_name" binding:"required,min=11,max=30"`
+	Password    string `form:"password" json:"password" binding:"required,min=2,max=40"`
 }
 
 func (service *UserLoginService) Login() (models.User, *serializer.Response) {}
