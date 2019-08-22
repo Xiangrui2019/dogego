580029f3a00be005e670a592e87c399333dfd13f
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 20:11:07 +0800

    updated profile sussceed not reload

--- services/update_profile_service.go
+++ services/update_profile_service.go
@@ -39,6 +39,6 @@ func (service *UpdateProfileService) Update(user *models.User) *serializer.Respo
 
 	return &serializer.Response{
 		Code:    http.StatusOK,
-		Message: "更新用户信息成功, 请重新登录.",
+		Message: "更新用户信息成功.",
 	}
 }
