ce1e22b86c120415b6662e9edb7216809373e048
Go Go Go <xiangrui@aiursoft.com>
2019-08-22 17:32:15 +0800

    sdfds

--- services/update_profile_service.go
+++ services/update_profile_service.go
@@ -6,6 +6,9 @@ import (
 )
 
 type UpdateProfileService struct {
+	NickName string `form:"nick_name" json:"nick_name" binding:"required,min=2,max=100"`
+	Bio      string `form:"bio" json:"bio" binding:"required,min=2,max=100"`
+	Avatar   string `form:"avatar" json:"avatar" binding:"required,min=2,max=200"`
 }
 
 func (service *UpdateProfileService) Update(user *models.User) *serializer.Response {
