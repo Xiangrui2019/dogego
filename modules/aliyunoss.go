2233101d30d84807ebd4cc1b3c625d25bbec4259
Go Go Go <xiangrui@aiursoft.com>
2019-08-25 08:49:37 +0800

    add aliyun oss bucket module

--- modules/aliyunoss.go
+++ modules/aliyunoss.go
@@ -8,11 +8,18 @@ import (
 )
 
 var AliyunOSSModule *oss.Client
+var AliyunOSSBucketModule *oss.Bucket
 
 func InitAliyunOSSModule() {
 	var err error
 
 	AliyunOSSModule, err = oss.New(os.Getenv("OSS_END_POINT"), os.Getenv("OSS_ACCESS_KEY_ID"), os.Getenv("OSS_ACCESS_KEY_SECRET"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	AliyunOSSBucketModule, err = AliyunOSSModule.Bucket(os.Getenv("OSS_BUCKET"))
 
 	if err != nil {
 		log.Fatal(err)
