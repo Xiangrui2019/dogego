40bd27b3cdf9a8b3701f01b1325a80fe239a9ce7
Go Go Go <xiangrui@aiursoft.com>
2019-08-25 14:21:37 +0800

    防止有人不会配置跨域

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -10,7 +10,11 @@ func Cors(cors_domain string) gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
-	config.AllowOrigins = []string{cors_domain}
+	if gin.Mode() != gin.ReleaseMode {
+		config.AllowAllOrigins = true
+	} else {
+		config.AllowOrigins = []string{cors_domain}
+	}
 	config.AllowCredentials = true
 	return cors.New(config)
 }
