f5ad8bee4bc1ece2f9bcff285b64b2338621af2a
Go Go Go <xiangrui@aiursoft.com>
2019-08-25 09:24:26 +0800

    add qiniu oss method

--- modules/qiniuossmodule.go
+++ modules/qiniuossmodule.go
@@ -1,6 +1,18 @@
 package modules
 
 import (
+	"os"
+
 	"github.com/qiniu/api.v7/auth/qbox"
-    "github.com/qiniu/api.v7/storage"
+	"github.com/qiniu/api.v7/storage"
 )
+
+var QiniuOSSModule *qbox.Mac
+var QiniuOSSBucketModule *storage.PutPolicy
+
+func InitQiniuOSSModule() {
+	QiniuOSSModule = qbox.NewMac(os.Getenv("QINIU_AK"), os.Getenv("QINIU_SK"))
+	QiniuOSSBucketModule = &storage.PutPolicy{
+		Scope: os.Getenv("QINIU_BUCKET"),
+	}
+}
