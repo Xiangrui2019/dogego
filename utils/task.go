554ac6933f5499eaab8153300d36ca1c06313373
Go Go Go <xiangrui@aiursoft.com>
2019-08-27 06:59:54 +0800

    remove task rpc

--- utils/task.go
+++ utils/task.go
@@ -1,17 +1,22 @@
 package utils
 
 import (
-	"net/http"
+	"log"
+	"reflect"
+	"runtime"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RunTask(context *gin.Context, job func() error) {
+	from := time.Now().UnixNano()
 	err := job()
-
+	to := time.Now().UnixNano()
+	jobName := runtime.FuncForPC(reflect.ValueOf(job).Pointer()).Name()
 	if err != nil {
-		context.String(http.StatusInternalServerError, err.Error())
+		log.Printf("%s 执行失败: %dms\n", jobName, (to-from)/int64(time.Millisecond))
 	} else {
-		context.String(http.StatusOK, "")
+		log.Printf("%s 执行成功: %dms\n", jobName, (to-from)/int64(time.Millisecond))
 	}
 }
