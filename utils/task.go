526b3f4df180ced33af866c8d4aa2efeebe51e81
Go Go Go <xiangrui@aiursoft.com>
2019-08-27 09:14:16 +0800

    for lock jobname get up

--- utils/task.go
+++ utils/task.go
@@ -8,10 +8,10 @@ import (
 )
 
 func RunTask(job func() error) {
+	jobName := runtime.FuncForPC(reflect.ValueOf(job).Pointer()).Name()
 	from := time.Now().UnixNano()
 	err := job()
 	to := time.Now().UnixNano()
-	jobName := runtime.FuncForPC(reflect.ValueOf(job).Pointer()).Name()
 	if err != nil {
 		log.Printf("%s Execute Error: %dms\n", jobName, (to-from)/int64(time.Millisecond))
 	} else {
