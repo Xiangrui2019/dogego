26cae8fc2a40842519950da1090751b61d39f204
Go Go Go <xiangrui@aiursoft.com>
2019-08-27 11:17:54 +0800

    add task to redis locker

--- utils/task.go
+++ utils/task.go
@@ -1,7 +1,7 @@
 package utils
 
 import (
-	"dogego/cache"
+	"dogego/modules"
 	"log"
 	"reflect"
 	"runtime"
@@ -11,16 +11,19 @@ import (
 func RunTask(job func() error) {
 	jobName := runtime.FuncForPC(reflect.ValueOf(job).Pointer()).Name()
 
-	err := cache.CacheClient.SetNX(jobName, "true", 0).Err()
-	if err != nil {
+	if !modules.LockerModule.Lock(jobName) {
 		return
 	}
+
 	from := time.Now().UnixNano()
-	err = job()
+	err := job()
 	to := time.Now().UnixNano()
+
 	if err != nil {
 		log.Printf("%s Execute Error: %dms\n", jobName, (to-from)/int64(time.Millisecond))
 	} else {
 		log.Printf("%s Execute Success: %dms\n", jobName, (to-from)/int64(time.Millisecond))
 	}
+
+	modules.LockerModule.Free(jobName)
 }
