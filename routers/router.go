f39d94314e099db8da8c2d82b24bbc21f86b7f33
Go Go Go <xiangrui@aiursoft.com>
2019-08-28 20:37:32 +0800

    fix: SESSION_SECRET

--- routers/router.go
+++ routers/router.go
@@ -12,7 +12,7 @@ func NewRouter() *gin.Engine {
 	router := gin.Default()
 
 	// 中间件, 顺序必须是这样
-	router.Use(middlewares.Session(os.Getenv("SESSION_SECERT")))
+	router.Use(middlewares.Session(os.Getenv("SESSION_SECRET")))
 	router.Use(middlewares.Cors(os.Getenv("CORS_DOMAIN")))
 	router.Use(middlewares.CurrentUser())
 
