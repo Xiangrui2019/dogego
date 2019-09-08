b77bd2f98faae8376067753195f94fe43a6b3dea
Go Go Go <xiangrui@aiursoft.com>
2019-09-08 09:26:30 +0800

    add health checks

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"dogego/modules"
 	"dogego/serializer"
 	"net/http"
 
@@ -8,6 +9,16 @@ import (
 )
 
 func Ping(context *gin.Context) {
+	err := modules.HealthChecksModule.Check()
+
+	if len(err) != 0 {
+		context.JSON(http.StatusInternalServerError, &serializer.Response{
+			Code:    http.StatusInternalServerError,
+			Message: "健康检查出错",
+			Data:    err,
+		})
+	}
+
 	context.JSON(http.StatusOK, &serializer.Response{
 		Code:    http.StatusOK,
 		Message: "Pong",
